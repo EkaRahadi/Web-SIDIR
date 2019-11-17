12348349aba183efb64418673953c759c8b71214
RizkyRockatansky <thenotoriousrizky@gmail.com>
2019-11-17 11:43:45 +0700

    slide Duwur

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -89,12 +89,48 @@
         </div>
     </header>
     <!-- ##### Header Area End ##### -->
-    
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <!-- ##### Hero Area Start ##### -->
+   <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <center>
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="assets/images/bg-img/17.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="assets/images/bg-img/17.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="assets/images/bg-img/17.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div></center>
     <div class="hero-area">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-12 col-lg-8">
+                <div class="col-8 col-lg-8">
                     <!-- Breaking News Widget -->
                     <div class="breaking-news-area d-flex align-items-center">
                         <div class="news-title">
