be518af5e759abf07866895dbf2137e3321a713c
EkaRahadi <ekarahadi98@gmail.com>
2019-11-28 08:08:28 +0700

    Tambahan Migrations

---
+++ database/migrations/2019_11_22_032910_create_pengaduan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengaduanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->bigIncrements('id_pengaduan');
+            $table->text('aduan');
+            $table->foreign('id_status')->references('id_status')->on('status');
+            $table->foreign('id_pengunjung')->references('id_pengunjung')->on('pengunjung');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+}
